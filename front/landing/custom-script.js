e953711e660ba032c45ae565dd3d731bee55f698
fortunto2 <info@3dstr.ru>
2021-03-27 04:33:18 +0500

    add metrika

--- front/landing/custom-script.js
+++ front/landing/custom-script.js
@@ -1,1 +1,11 @@
-console.log(`Hello! I am running from a script injected on the page by Loconotion 🎉`);
+<!-- Yandex.Metrika counter -->
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(74733217, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
