9fa42594e7d3de7b568a490a6862a5f8c0f8fbe6
fortunto2 <info@3dstr.ru>
2022-02-09 03:59:19 +0500

    fix http get

--- tests/simple.py
+++ tests/simple.py
@@ -6,7 +6,9 @@ import requests
 with open('tests/data/measurement.json') as json_file:
     data = json.load(json_file)
 
-    # print(data)
+    print(data)
 
-r = requests.post("http://localhost:8000/upload_measurement", json=data)
+# проблема с тем что редирект в nginx переделывался на get, из за https
+r = requests.post("https://api.bashair.ru/upload_measurement", json=data)
+# r = requests.post("http://localhost:8000/upload_measurement", json=data)
 pprint(r.json())
