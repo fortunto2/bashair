8b6f11e3105124008ff9c8d37ff3b93644cbd326
50bytes.dev <50bytes.dev@gmail.com>
2022-02-12 09:37:29 +0500

    fix

--- back/schemas/city.py
+++ back/schemas/city.py
@@ -3,15 +3,16 @@ from typing import Optional
 from pydantic import BaseModel, AnyUrl, EmailStr
 
 
-class City(BaseModel):
-
-    id: int
+class CityBase(BaseModel):
     display_name: str
     latitude: Optional[float]
     longitude: Optional[float]
     # region: Optional[str]
     # country: Optional[str]
 
+
+class CityGet(CityBase):
+    id: int
+
     class Config:
         orm_mode = True
-
