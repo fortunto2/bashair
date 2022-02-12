5c904c12513659196f4043e44cc009935ca73521
fortunto2 <info@3dstr.ru>
2022-02-12 09:50:00 +0500

    fix merge

--- back/api/node.py
+++ back/api/node.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 
 from back.models.node import Node
 from back.schemas.node import NodePointGet
+from back.utils.exceptions import NotFound
 
 router = APIRouter(tags=["node"], prefix="/node")
 
@@ -23,13 +24,20 @@ def get_nodes():
     return nodes
 
 
-@router.get('/{node_id}/')
-def get_node(node_id: int):
-    # данные по датчику
-    pass
+@router.get('/{node_uid}/', response_model=NodePointGet)
+def get_node(node_uid: str):
+    """
+    Данные по датчику
+    """
+    try:
+        node = Node.objects.get(uid=node_uid)
+    except Node.DoesNotExist:
+        raise NotFound
+
+    return node
 
 
-@router.get('/{node_id}/history/')
+@router.get('/{node_uid}/history/')
 def get_node_history(node_id: int):
     # график из инфлюкса
     pass
