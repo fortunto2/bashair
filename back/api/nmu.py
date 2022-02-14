32af170533c59db312a0858a4c9fa68000f1453e
fortunto2 <info@3dstr.ru>
2022-02-14 10:34:02 +0500

    fix nmu

--- back/api/nmu.py
+++ back/api/nmu.py
@@ -1,6 +1,7 @@
-from typing import Optional
+from typing import Optional, List
 
 from fastapi import APIRouter
+from influxdb_client.client.flux_table import FluxTable, FluxRecord
 from starlette.responses import JSONResponse
 
 from back.schemas.nmu import NmuBase
@@ -13,15 +14,13 @@ def get_nmu_influx(measurment='predictions', start='-24h', city_id=None, region_
 
     city_q = ''
     region_q = ''
-    result = ''
-    time = ''
 
     if region_id:
         region_q = f'|> filter(fn: (r) => r["region_id"] == "{region_id}")'
     if city_id:
         city_q = f'|> filter(fn: (r) => r["city_id"] == "{city_id}")'
 
-    tables = query_api.query(
+    result: List[FluxTable] = query_api.query(
         f"""
         from(bucket: "air")
           |> range(start: {start})
@@ -29,31 +28,34 @@ def get_nmu_influx(measurment='predictions', start='-24h', city_id=None, region_
           |> filter(fn: (r) => r["_field"] == "nmu")
           {region_q}
           {city_q}
+          |> group(columns: ["_field"])
           |> last()
         """
     )
 
-    if tables:
-        result = [table.records[0]['_value'] for table in tables]
-        time = [table.records[0]['_time'] for table in tables]
-        print(result, time)
+    results = []
+    table: FluxTable
+    for table in result:
+        record: FluxRecord
+        for record in table.records:
+            results.append((record.get_value(), record.get_time()))
 
-    result_last = result[-1]
-    time_last = time[-1]
-
-    return result_last, time_last
+    if results:
+        return results[-1]
 
 
 @router.get('', response_model=NmuBase)
 def get_nmu(region_id: Optional[str] = '', city_id: Optional[str] = ''):
 
-    result, time = get_nmu_influx(city_id=city_id, region_id=region_id)
-    if time:
+    result = get_nmu_influx(city_id=city_id, region_id=region_id)
+    mode = result[0]
+    time = result[1]
+    if result:
 
         print(f'get NMU for {region_id}, {city_id} = {result}')
 
         return {
-            'mode': result,
+            'mode': mode,
             'datetime': time,
             'time': time.strftime("%H:%M"),
             'date': time.strftime("%Y-%m-%d"),
